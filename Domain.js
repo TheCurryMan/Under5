215c6e4136ebb50f06580b1728bc280c0b15d145
Avinash Jain <avinashkalpitjain@gmail.com>
2016-02-13 23:30:15 +0000

    Creating Ranges

---
+++ Domain.js
@@ -0,0 +1,48 @@
+var range = [["Dairy",["Dead", "Big", "You force one cow to make the milk for three"]], 
+["Chemicals",["Big", "Big", "Use chemicals and start WWIII"]]], 
+["Aerospace", ["Dead", "Big", "find two new buildings"]]
+["", [""]]
+Agriculture
+
+Crops
+
+Airplanes
+
+Banking
+
+Computers
+
+Education
+
+Electronics
+
+Energy
+
+Fashion
+
+Beverages
+
+Health
+
+Insurance
+
+Legal Services
+
+Manufacturing
+
+Media
+ 
+Music
+
+Tourism
+
+Transportation
+
+Travel
+
+Utilities
+
+Video games
+
+
+
